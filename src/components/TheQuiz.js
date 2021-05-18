893168a97b0707c813bbc4074295707c69beb7be
powsitu <sendtopow@gmail.com>
2021-05-18 11:48:07 +0200

    created basic quiz page with a basic quiz component

---
+++ src/components/TheQuiz.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./TheQuiz.css";
+
+export default function TheQuiz(props) {
+  return (
+    <div className="thequiz-container">
+      <div>
+        <img
+          src="https://source.unsplash.com/1600x900/?"
+          alt="quiz"
+          className="quiz-img"
+        />
+      </div>
+      <div className="quiz-question">This is very difficult question?</div>
+      <div>
+        <ul className="quiz-answers">
+          <li onClick={() => props.clickHandler("1")}>Answer 1</li>
+          <li onClick={() => props.clickHandler("2")}>Answer 2</li>
+          <li onClick={() => props.clickHandler("3")}>Answer 3</li>
+          <li onClick={() => props.clickHandler("4")}>Answer 4</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,7 +1,18 @@
+import TheQuiz from "../components/TheQuiz";
+
 export default function QuizPage() {
+  function onClickAnswer(answerId) {
+    console.log(`You clicked on answer ${answerId}`);
+  }
+
   return (
-    <div>
-      <h1>QuizPage</h1>
+    <div className="quiz-container">
+      <div className="quiz-timer">
+        <h1>30 seconds</h1>
+      </div>
+      <div className="quiz">
+        <TheQuiz clickHandler={onClickAnswer} />
+      </div>
     </div>
   );
 }
