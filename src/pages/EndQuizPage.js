36e289cef59b4180bd1de9763320a6dfb24c1655
powsitu <sendtopow@gmail.com>
2021-05-19 15:18:15 +0200

    added basic stuff on the page: game over message, the score and an input field with a button to submit

--- src/pages/EndQuizPage.js
+++ src/pages/EndQuizPage.js
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectScore } from "../store/gameState/selectors";
+
 export default function EndQuizPage() {
+  const score = useSelector(selectScore);
+  const [playerName, set_playerName] = useState("");
   return (
-    <div>
-      <h1>EndQuizPage</h1>
+    <div className="thequiz-container">
+      <h1>Game Over</h1>
+      <h2>You have scored {score} points!</h2>
+      <h3>Type your name to enter our hall of fame</h3>
+      <input type="text" placeholder="Your name..." />
+      <button>Submit</button>
     </div>
   );
 }
