bafcc05d0824cf0e24057a90f62ae26d3ab41a0e
powsitu <sendtopow@gmail.com>
2021-05-19 15:25:18 +0200

    added basic styling to the input field

--- src/pages/EndQuizPage.js
+++ src/pages/EndQuizPage.js
@@ -5,12 +5,23 @@ import { selectScore } from "../store/gameState/selectors";
 export default function EndQuizPage() {
   const score = useSelector(selectScore);
   const [playerName, set_playerName] = useState("");
+
+  function nameChangeHandler(event) {
+    set_playerName(event.target.value);
+  }
+
   return (
     <div className="thequiz-container">
       <h1>Game Over</h1>
       <h2>You have scored {score} points!</h2>
       <h3>Type your name to enter our hall of fame</h3>
-      <input type="text" placeholder="Your name..." />
+      <input
+        className="quiz-playerInput"
+        type="text"
+        placeholder="Your name..."
+        value={playerName}
+        onChange={(event) => nameChangeHandler(event)}
+      />
       <button>Submit</button>
     </div>
   );
