7f20a2a56a2404aeeafb4b610f2cd40b4f72daed
MARIAJULIASOLER <JULIASOLERNUNES@YMAIL.COM>
2021-05-21 13:49:45 +0200

    Final bug with scrwolling was fixed and the read me file was created

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,14 @@
 import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
-// import { selectAllHighscores } from "../store/highscores/selectors";
-// import { fetchHighscores } from "../store/highscores/actions";
 
 import SoundPlayer from "../components/SoundPlayer";
 
 import "../Css/HomePage.css";
 
 export default function HomePage() {
-  // const dispatch = useDispatch();
-  // const highscore = useSelector(selectAllHighscores);
-  // const [highscores, setHighScores] = useState([]);
-
   useEffect(() => {
     SoundPlayer("HOMEPAGE");
   }, []);
-
-  // useEffect(() => {
-  //   dispatch(fetchHighscores());
-  // }, [dispatch]);
-  // useEffect(() => {
-  //   setHighScores(highscore);
-  // }, [highscore]);
 
   return (
     <div id="image">
